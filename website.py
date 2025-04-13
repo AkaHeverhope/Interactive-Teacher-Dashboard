6fd71e540a55fa8c704ea14fc113bdc3ccd86b1b
AkaHeverhope <akasaransam@gmail.com>
2025-04-13 10:37:38 +0530

    Update website.py

--- website.py
+++ website.py
@@ -3,14 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
-import plotly.graph_objects as go
-from datetime import datetime
-import io
-import base64
 from PIL import Image
-import time
-import random
 import altair as alt
 from streamlit_option_menu import option_menu
 
